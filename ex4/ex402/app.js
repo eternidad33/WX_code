cfbedd8a9c3a7f02226cfb07016adc695655886d
vigilr <3496655347@qq.com>
2020-04-17 14:15:52 +0800

    执行顺序

---
+++ ex4/ex402/app.js
@@ -0,0 +1,13 @@
+//app.js
+App({
+  //
+  onLaunch: function() {
+    console.log("【app.js】小程序初始化")
+  },
+  onShow: function() {
+    console.log("【app.js】小程序显示")
+  },
+  onHide: function() {
+    console.log("【app.js】小程序隐藏")
+  }
+})
---
+++ ex4/ex402/pages/index/index.js
@@ -0,0 +1,18 @@
+//index.js
+Page({
+  onLoad: function() {
+    console.log("【index.js】onLoad页面加载")
+  },
+  onShow: function() {
+    console.log("【index.js】onShow页面显示")
+  },
+  onReady: function() {
+    console.log("【index.js】onRead页面渲染")
+  },
+  onHide: function() {
+    console.log("【index.js】onHide页面隐藏")
+  },
+  onUnload: function() {
+    console.log("【index.js】onUnLoad页面卸载")
+  }
+})
