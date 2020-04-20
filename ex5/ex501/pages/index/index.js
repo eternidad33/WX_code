af0c1970af70ac8e423553067fedaf775adbd02b
vigilr <3496655347@qq.com>
2020-04-20 21:51:38 +0800

    js if

--- ex5/ex501/pages/index/index.js
+++ ex5/ex501/pages/index/index.js
@@ -4,11 +4,28 @@ const app = getApp()
 
 Page({
   data: {
-    score: 0
+    score: 0,
+    grade: "及格"
   },
   scoreInput: function(e) {
+    var score = e.detail.value;
+    var grade;
+    if (score > 100 || score < 0) {
+      grade = '成绩输入有误!'
+    } else if (score > 90) {
+      grade = '优秀!';
+    } else if (score > 80) {
+      grade = '良好!';
+    } else if (score > 70) {
+      grade = '中等!';
+    } else if (score >= 60) {
+      grade = '及格!';
+    } else {
+      grade = '不及格！';
+    }
     this.setData({
-      score: e.detail.value
+      score: score,
+      grade: grade
     })
   }
 })
